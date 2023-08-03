e7e8f2bced21b8e1afd3fbe92913159209cd5870
Mika <mickaelklein@hotmail.fr>
2023-08-03 20:46:47 +0200

    Redux store updated

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -22,30 +22,10 @@ const appDataSlice = createSlice({
 const userInteractionSlice = createSlice({
   name: "userInteraction",
   initialState: {
-    modalDisplayed: false,
-    stateDropdownDisplayed: false,
-    departmentDropdownDisplayed: false,
-    birthCalendarDisplayed: false,
-    startCalendarDisplayed: false,
     page: null,
     pageTitle: null,
   },
   reducers: {
-    toogleModal: (state) => {
-      state.modal = !state.modal;
-    },
-    toogleStateDropdown: (state) => {
-      state.stateDropdownDisplayed = !state.stateDropdownDisplayed;
-    },
-    toogleDepartmentDropdown: (state) => {
-      state.departmentDropdownDisplayed = !state.departmentDropdownDisplayed;
-    },
-    toogleBirthCalendar: (state) => {
-      state.birthCalendarDisplayed = !state.birthCalendarDisplayed;
-    },
-    toogleStartCalendar: (state) => {
-      state.startCalendarDisplayed = !state.startCalendarDisplayed;
-    },
     updatePageLocation: (state, action) => {
       state.page = action.payload.page;
       state.pageTitle = action.payload.pageTitle;
